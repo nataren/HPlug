665964a3f4eba89b708430dc15c506ec16a2c1f2
Cesar Lopez-Nataren <cesarn@mindtouch.com>
2014-01-28 11:04:05 -0800

    Change data model a bit. Integrate Conduit.HTTP as underlying HTTP networking library

--- plug.hs
+++ plug.hs
@@ -1,82 +1,42 @@
-joinString :: String -> [String] -> String
-joinString _ [] = ""
-joinString sep [ value ] = value
-joinString sep (value:values) = value ++ sep ++ joinString sep values
-
--- let p = Plug "http" "example.com" 80 True Nothing Nothing Nothing
--- p `withScheme` "https" `withPort` 443 `at` "x" `at` "y" `with` ("a", Just "b") `withFragment` "foo"
-
+module MindTouch.Http(
+  defaultPlug,
+  Plug,
+  Response,
+  withScheme,
+  withHostname,
+  withPort,
+  withTrailingSlash,
+  withoutTrailingSlash,
+  with,
+  at,
+  usesDefaultPort,
+  withCredentials,
+  get,
+  testPlug) where
+import qualified Network.HTTP.Conduit as Conduit
+import Data.Conduit
+import qualified Data.ByteString.Lazy as L
+import Network (withSocketsDo)
+import Data.List (intersperse)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe
+import Control.Monad.IO.Class (liftIO)
 
 data Plug = Plug {
-    scheme          :: String,
-    hostname        :: String,
-    port            :: Int,
-    usesDefaultPort :: Bool,
-    path            :: Maybe ([String], Bool),
-    query           :: Maybe [(String, Maybe String)],
-    fragment        :: Maybe String
-} deriving (Eq)
+  scheme        :: String,
+  hostname      :: String,
+  port          :: Int,
+  path          :: [String],
+  query         :: [(String, Maybe String)],
+  fragment      :: Maybe String,
+  trailingslash :: Bool,
+  username      :: Maybe String,
+  password      :: Maybe String
+} deriving(Eq)
 
---(->) :: Plug -> (Plug -> Plug) -> Plug
---(->) p f = f p
-
--- (Plug scheme hostname port usesDefaultPort path query fragment) = Plug scheme hostname port usesDefaultPort path query fragment
-
--- TODO (steveb): comparison needs to be case-insensitive
-isDefaultPort :: String -> Int -> Bool
-isDefaultPort "http" 80 = True
-isDefaultPort "https" 443 = True
-isDefaultPort "ftp" 21 = True
-isDefaultPort _ _ = False
-
-withScheme :: Plug -> String -> Plug
-withScheme (Plug _ hostname port _ path query fragment) scheme = Plug scheme hostname port (isDefaultPort scheme port) path query fragment
-
-withHostname :: Plug -> String -> Plug
-withHostname (Plug scheme _ port usesDefaultPort path query fragment) hostname = Plug scheme hostname port usesDefaultPort path query fragment
-
-withPort :: Plug -> Int -> Plug
-withPort (Plug scheme hostname _ _ path query fragment) port = Plug scheme hostname port (isDefaultPort scheme port) path query fragment
-
-at :: Plug -> String -> Plug
-at (Plug scheme hostname port usesDefaultPort Nothing query fragment) segment = 
-    Plug scheme hostname port usesDefaultPort (Just ([ segment ], False)) query fragment
-at (Plug scheme hostname port usesDefaultPort (Just (segments, trailingSlash)) query fragment) segment = 
-    Plug scheme hostname port usesDefaultPort (Just (segments ++ [ segment ], trailingSlash)) query fragment
-
-withTrailingSlash :: Plug -> Plug
-withTrailingSlash (Plug scheme hostname port usesDefaultPort Nothing query fragment) = Plug scheme hostname port usesDefaultPort (Just ([], True)) query fragment
-withTrailingSlash (Plug scheme hostname port usesDefaultPort (Just (segments, _)) query fragment) = Plug scheme hostname port usesDefaultPort (Just (segments, True)) query fragment
-
-withoutTrailingSlash :: Plug -> Plug
-withoutTrailingSlash (Plug scheme hostname port usesDefaultPort (Just (segments, True)) query fragment) = Plug scheme hostname port usesDefaultPort (Just (segments, False)) query fragment
-withoutTrailingSlash p = p
-
-
-with :: Plug -> (String, Maybe String) -> Plug
-with (Plug scheme hostname port usesDefaultPort path Nothing fragment) kv = 
-    Plug scheme hostname port usesDefaultPort path (Just [ kv ]) fragment
-with (Plug scheme hostname port usesDefaultPort path (Just kvs) fragment) kv = 
-    Plug scheme hostname port usesDefaultPort path (Just (kvs ++ [ kv ])) fragment
-
-withParams :: Plug -> [(String, Maybe String)] -> Plug
-withParams (Plug scheme hostname port usesDefaultPort path Nothing fragment) kvs = 
-    Plug scheme hostname port usesDefaultPort path (Just kvs) fragment
-withParams (Plug scheme hostname port usesDefaultPort path (Just kvs1) fragment) kvs2 = 
-    Plug scheme hostname port usesDefaultPort path (Just (kvs1 ++ kvs2)) fragment
-
--- withQuery
--- withoutQuery
--- getParam
--- getParams
-
--- without key (Plug scheme hostname port usesDefaultPort path query fragment) = Plug scheme hostname port usesDefaultPort path query fragment
-
-withFragment :: Plug -> String -> Plug
-withFragment (Plug scheme hostname port usesDefaultPort path query _) fragment = Plug scheme hostname port usesDefaultPort path query (Just fragment)
-
-withoutFragment :: Plug -> Plug
-withoutFragment (Plug scheme hostname port usesDefaultPort path query _) = Plug scheme hostname port usesDefaultPort path query Nothing
+join :: [a] -> [[a]] -> [a]
+join delim l = concat (intersperse delim l)
 
 -- TODO: missing implementation for encoding
 encodeSegment :: String -> String
@@ -91,23 +51,115 @@ encodeFragment :: String -> String
 encodeFragment text = text
 
 getHost :: Plug -> String
-getHost (Plug _ hostname port True _ _ _) = hostname
-getHost (Plug _ hostname port False _ _ _) = hostname ++ ":" ++ show port
+getHost plug = hostname plug ++ (if usesDefaultPort plug then [] else ":" ++ show (port plug))
 
 getPath :: Plug -> String
-getPath (Plug _ _ _ _ Nothing _ _) = ""
-getPath (Plug _ _ _ _ (Just (segments, True)) _ _) = '/' : joinString "/" (map encodeSegment segments) ++ "/"
-getPath (Plug _ _ _ _ (Just (segments, False)) _ _) = '/' : joinString "/" (map encodeSegment segments)
+getPath plug = '/' : join "/" (map encodeSegment (path plug)) ++ (if trailingslash plug then "/" else "")
 
 getQuery :: Plug -> String
-getQuery (Plug _ _ _ _ _ Nothing _) = ""
-getQuery (Plug _ _ _ _ _ (Just kvs) _) = '?' : joinString "&" (map showQueryKeyValue kvs)
-    where showQueryKeyValue (k, Nothing) = encodeQuery k
-          showQueryKeyValue (k, (Just v)) = encodeQuery k ++ "=" ++ encodeQuery v
+getQuery plug = join "&" (map tostring (query plug))
+  where
+    tostring query' = case query' of
+      (name, Nothing) -> encodeQuery name
+      (name, Just value) -> encodeQuery name ++ "=" ++ encodeQuery value
 
 getFragment :: Plug -> String
-getFragment (Plug _ _ _ _ _ _ Nothing) = ""
-getFragment (Plug _ _ _ _ _ _ (Just f)) = '#' : encodeFragment f
-
+getFragment plug = case fragment plug of
+  Nothing -> ""
+  Just fragment' -> '#' : encodeFragment fragment'
+  
 instance Show Plug where
-    show p = scheme p ++ "://" ++ getHost p ++ getPath p ++ getQuery p ++ getFragment p
+  show p = scheme p ++ "://" ++ getHost p ++ getPath p ++ "?" ++ getQuery p ++ getFragment p
+
+
+data Status = OK
+data Response = Response {
+  status      :: Status
+}
+  
+defaultPlug :: Plug
+defaultPlug     = Plug {
+  scheme        = "http",
+  hostname      = "localhost",
+  port          = 80,
+  path          = [],
+  query         = [],
+  fragment      = Nothing,
+  trailingslash = False,
+  username      = Nothing,
+  password      = Nothing
+}
+
+withScheme :: Plug -> String -> Plug
+withScheme plug scheme' = plug { scheme = scheme' }
+  
+withHostname :: Plug -> String -> Plug
+withHostname plug hostname' = plug { hostname = hostname' }
+
+withPort :: Plug -> Int -> Plug
+withPort plug port' = plug { port = port' }
+
+withTrailingSlash :: Plug -> Plug
+withTrailingSlash plug = plug { trailingslash = True }
+
+withoutTrailingSlash :: Plug -> Plug
+withoutTrailingSlash plug = plug { trailingslash = False }
+
+withCredentials :: Plug -> (String, String) -> Plug
+withCredentials plug (username', password') = plug {
+  username = Just username',
+  password = Just password'
+}
+
+at :: Plug -> String -> Plug
+at plug segment = plug { path = (path plug) ++ [segment] }
+
+with :: Show a => Plug -> (String, Maybe a) -> Plug
+with plug kvp =
+  let (key, value) = kvp in
+    plug { query = (query plug) ++ [(key, convert value)] }
+  where
+    convert value = case value of
+      Nothing -> Nothing
+      Just v -> Just (show v) 
+    
+-- TODO (steveb): comparison needs to be case-insensitive
+isDefaultPort :: String -> Int -> Bool
+isDefaultPort "http" 80 = True
+isDefaultPort "https" 443 = True
+isDefaultPort "ftp" 21 = True
+isDefaultPort _ _ = False
+
+usesDefaultPort :: Plug -> Bool
+usesDefaultPort plug = isDefaultPort (scheme plug) (port plug)
+
+getAuthRequest :: Maybe String -> Maybe String -> Conduit.Request -> Conduit.Request
+getAuthRequest username' password' request' =
+  case (username', password') of
+    (Just u, Just p) -> Conduit.applyBasicAuth (BC.pack u) (BC.pack p) request'
+    (_, _) -> request'
+
+get :: Plug -> IO (Conduit.Response (ResumableSource (ResourceT IO) BC.ByteString))
+get plug = withSocketsDo $ Conduit.withManager $ \manager -> do
+  request <- Conduit.parseUrl (show plug)
+  let request' = getAuthRequest (username plug) (password plug) request
+  response <- Conduit.http request' manager
+  return response
+  
+-- post :: Plug -> Response
+-- put :: Plug -> Response
+
+
+testPlug :: Plug
+testPlug =
+  defaultPlug
+  `withHostname` "joelsite.mindtouch.us"
+  `withPort` 80
+  `withScheme` "http"
+  `at` "a"
+  `at` "b"
+  `at` "c"
+  `with` ("pageid", Just 1 :: Maybe Int)
+  `with` ("recursive", Just True)
+  `with` ("emptyparam", Nothing :: Maybe String)
+  `withCredentials` ("cesarn", "fooboo")
